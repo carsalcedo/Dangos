f10b845d59a97990e8a69f5dd80f140a33af87b1
carsalcedo <fercarrera07@gmail.com>
2022-01-30 20:51:46 -0400

    primera fase de maquetado

---
+++ js/action.js
@@ -0,0 +1,63 @@
+let searchform = document.querySelector('.searchFormContainer');
+
+/*funcion para aparecer barra de busqueda*/
+document.querySelector('#search-btn').onclick = () =>{
+    searchform.classList.toggle('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+/*funcion para aparecer shopping car*/
+let cart = document.querySelector('.shoppingCartContainer');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cart.classList.toggle('active');
+    searchform.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+/*funcion para aparecer login form*/
+let loginForm = document.querySelector('.loginFormContainer');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchform.classList.remove('active');
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+/*funcion para aparecer menu responsive*/
+
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+    searchform.classList.remove('active');
+    cart.classList.remove('active');
+
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active')
+}
+
+//effect to section start image
+
+document.querySelector('.home').onmousemove = (e) =>{
+    let x = (window.innerWidth - e.pageX * -5) / 90;
+    let y = (window.innerHeight - e.pageY * -5) / 90;
+
+    document.querySelector('.home .home-parallax-img').style.transform = `translateX(${y}px)
+    translateY(${x}px)`; 
+}
+
+document.querySelector('.home').onmouseleave = () =>{
+
+    document.querySelector('.home .home-parallax-img').style.transform = `translateX(0px)
+    translateY(0px)`; 
+}
+
+

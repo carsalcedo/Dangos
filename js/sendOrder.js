0dd65470b3ec25be6002d8e1c35be8febea785ef
carsalcedo <fercarrera07@gmail.com>
2022-02-08 13:21:52 -0400

    funcion enviar correo agregada

---
+++ js/sendOrder.js
@@ -0,0 +1,61 @@
+/*const formOrder = document.getElementById('formOrder');
+function sendEmail(){
+    let temParams={
+        name: document.getElementById('name').value,
+        orden: document.getElementById('orden').value,
+        much: document.getElementById('much').value,
+        tel: document.getElementById('tel').value,
+        address: document.getElementById('address').value,
+        details: document.getElementById('details').value,
+        dates: document.getElementById('dates').value,
+    };
+
+    const serviceID = 'default_service';
+    const templateID = 'template_3b8pu0t';
+
+    emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+        document.getElementById('mensaje-enviado').classList.add('active');
+      }, (err) => {
+        alert(JSON.stringify(err));
+      });
+}
+
+formOrder.addEventListener('submit', (e) => {
+    e.preventDefault();
+    sendEmail()  
+});*/
+
+
+const btn = document.getElementById('button');
+
+document.getElementById('formOrder')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+   document.getElementById('formOrder').reset();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_3b8pu0t';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      document.getElementById('mensaje-enviado').classList.add('active');
+        btn.value = 'proceed to order';
+
+        setTimeout(() => {
+            document.getElementById('mensaje-enviado').classList.remove('active');
+        }, 2000);
+
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+
+
+
+
+
